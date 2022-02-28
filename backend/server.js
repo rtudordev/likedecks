2d1a1c83049700ee2f1b83d6f38a707897a9a064
rtudordev <rtudordev@gmail.com>
2022-02-28 18:34:06 +0000

    initial api setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// welcome message for root url
+app.get("/", (req, res) => {
+  //res.status(200).json({ message: "Welcome to the likedecks API" });
+  res.status(200).send("Welcome to the likedecks API");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
